966ccf1fb3f43a7c2dfacc4161adb913b7a4ca8e
cyrilforrez <cyilforrez@gmail.com>
2017-07-21 15:49:35 +0200

    Add comments

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
+/*-----------------------------------Declaration of variable--------------------------------*/
 var img = ['img/pc/DOFUS.png', 'img/pc/dofuspets.jpg', 'img/pc/wakfu.png'];
 var monImage = document.getElementById("puzzle");
 cont = 0;
+var interv = setInterval(nextimg, 2000);
 
-var interv = setInterval(nextimg,2000);
 
-function nextimg() {
+//-----------------------Next picture function---------------------------
+function nextimg() { //------------The click passes to the next image
     if (cont >= 2) {
         cont = img.length - 4;
     }
     cont++;
     monImage.src = img[cont];
-    clearInterval(interv);
+    clearInterval(interv);  //------------Stop the set interval
     interv = setInterval(nextimg, 2000);
 }
 
-function previmg() {
+//----------------------Previous image function---------------------
+function previmg() {  //----------The click returns to the previous image
     if (cont <= 0) {
         cont = img.length;
     }
     cont--;
     monImage.src = img[cont];
-    clearInterval(interv);
+    clearInterval(interv);  //-----------Stop the set interval
     interv = setInterval(nextimg, 2000);
 }
-
